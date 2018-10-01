0db727da9ec1549f1019b6db752d4a1514e5e7f7
Sam Mulube <sam@umbrellium.co.uk>
2018-10-01 16:53:55 +0100

    Update setup.py to read readme and a few other tweaks

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='iotstore_client',
@@ -7,8 +10,16 @@ setup(
     maintainer_email='sam@thingful.net',
     url='https://github.com/thingful/iotstore_client_py',
     description='Python client for the DECODE encrypted datastore',
-    packages=['iotstore_client'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
     install_requires=[
         'protobuf',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
     ],
 )
